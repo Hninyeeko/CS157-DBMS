44c0355f099537fe45ea09d947f488112300bd06
KunalPradhan1 <116305812+KunalPradhan1@users.noreply.github.com>
2024-05-03 19:00:04 -0700

    start of everything
    this has some sort of setup for connection with mysql still working on the create sql statements

---
+++ listing/src/app/database/page.tsx
@@ -0,0 +1,78 @@
+import mysql from 'mysql2/promise';
+import fs from 'fs/promises';
+export let connection;
+
+
+
+export async function connect() {
+  let connection;
+  try {
+      connection = await mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: '',
+          port: 3307,
+          database: 'users'
+      });
+      console.log('Connected to the MySQL server.');
+  } catch (error) {
+      console.error(`Error connecting to the MySQL server`);
+      throw error; // Throw the error to stop execution
+  }
+  return connection;
+}
+
+// This was just a test for me to make sure the connection was working lowkey 
+export async function selectAllStudents() {
+  try {
+    // Ensure connection is established
+    if (!connection) {
+      connection = await connect();
+    }
+    const [rows, fields] = await connection.query('SELECT * FROM students');
+    console.log(rows);
+    console.log('Query executed successfully.');
+    return rows;
+  } catch (error) {
+    console.error(`Error executing query: `, error);
+  }
+ 
+}
+// ignore this i was just testing the connection
+
+
+
+
+// this is still a work in progress
+async function executeSqlFile(filePath) {
+  // Create a connection to the database
+  const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      port: 3307,
+  });
+
+  try {
+      // Read the SQL file
+      const sql = await fs.readFile(filePath, 'utf-8');
+
+      // Execute the SQL commands
+      const [results] = await connection.query(sql);
+
+      console.log('SQL file executed successfully.');
+  } catch (error) {
+      console.error(`Error executing SQL file: `, error);
+  } finally {
+      // Close the database connection
+      await connection.end();
+  }
+}
+
+
+// Call the function with the path to your SQL file
+executeSqlFile('.\\src\\dbsetup.sql');
+  
+ 
+ 
+
--- listing/src/app/page.tsx
+++ listing/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import {connect} from "./database/page";
+import {selectAllStudents} from "./database/page";
 
-export default function Home() {
+let connection1 = connect();
+selectAllStudents();
+
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p> hello world!</p>
