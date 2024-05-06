5dc889095595213e418a0ec5b0404812e0ca51d7
paingheinkyaw1 <98957792+paingheinkyaw1@users.noreply.github.com>
2024-05-05 22:56:28 -0700

    Login Page Complete

--- listing/src/app/loginPage/page.tsx
+++ listing/src/app/loginPage/page.tsx
@@ -14,14 +14,14 @@ const FormInput: React.FC<FormInputProps> = ({ type, placeholder, name, classNam
 
 const MyComponent: React.FC = () => {
   return (
-    <div className="box-border relative overflow-hidden w-full min-h-screen">
+    <div className="box-border relative overflow-hidden mt-5 w-full min-h-screen">
       <img
         src="https://cdn.builder.io/api/v1/image/assets%2F25fd6ed351994175ac507a1dcdcba146%2F3cb9462f99064364b1c99132a8587f9c"
         alt=""
         className="object-cover absolute inset-0 w-full h-full"
       />
       <div className="box-border absolute inset-0 flex flex-col justify-center px-5 pb-4 mx-auto text-xs" style={{ maxWidth: 1200 }}>
-        <header className="box-border relative pt-10 pb-4 text-center">
+        <header className="box-border relative pb-4 text-center">
           <h1 style={{ fontSize: "100px" }}>Cartier</h1>
         </header>
         <div className="box-border relative self-center mt-5 text-4xl">
@@ -42,12 +42,20 @@ const MyComponent: React.FC = () => {
             name="Password"
             className="box-border flex relative flex-col shrink-0 py-2.5 pr-16 pl-2.5 mt-5 rounded border border-solid border-stone-300"
           />
-          <button
-            type="submit"
-            className="box-border relative shrink-0 px-6 py-4 mt-5 text-center rounded appearance-none cursor-pointer bg-[black] text-[white]"
-          >
-            Login
-          </button>
+          <div className="flex mt-5 space-x-4">
+            <button
+              type="submit"
+              className="box-border flex-1 px-6 py-4 text-center rounded appearance-none cursor-pointer bg-[black] text-[white]"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="box-border flex-1 px-6 py-4 text-center rounded appearance-none cursor-pointer bg-[black] text-[white]"
+            >
+              Signup
+            </button>
+          </div>
         </form>
       </div>
     </div>
